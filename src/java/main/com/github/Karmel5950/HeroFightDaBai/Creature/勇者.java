c61c17f4bb18acc8f43de121dad0178b7a57a28a
Karmel5950 <gaoang42@outlook.com>
2021-06-22 09:24:15 +0800

    实现了勇者属性的初始化

--- src/java/main/com/github/Karmel5950/HeroFightDaBai/Creature/勇者.java
+++ src/java/main/com/github/Karmel5950/HeroFightDaBai/Creature/勇者.java
@@ -2,13 +2,13 @@ package com.github.Karmel5950.HeroFightDaBai.Creature;
 
 public class 勇者 extends 生物{
 
-    {
-        super.初始生命值 = 100;
-        super.初始攻击力 = 10;
-        super.初始防御力 = 0;
-        super.生命值成长 = 10;
-        super.攻击力成长 = 1;
-        super.防御力成长 = 1;
+    @Override
+    protected void 初始化() {
+        this.初始生命值 = 100;
+        this.初始攻击力 = 10;
+        this.初始防御力 = 0;
+        this.生命值成长 = 10;
+        this.攻击力成长 = 1;
+        this.防御力成长 = 1;
     }
-
 }
--- src/java/main/com/github/Karmel5950/HeroFightDaBai/Creature/生物.java
+++ src/java/main/com/github/Karmel5950/HeroFightDaBai/Creature/生物.java
@@ -21,6 +21,7 @@ public class 生物 {
     }
     public 生物(int 等级) {
         this.等级 = 等级 ;
+        this.初始化();
         this.生命值最大值 = this.初始生命值 + 等级 * this.生命值成长;
         this.生命值当前值 = this.生命值最大值;
         this.攻击力 = this.初始攻击力 + 等级 * this.攻击力成长;
@@ -33,5 +34,13 @@ public class 生物 {
 
     }
 
+    protected void 初始化(){
+        this.初始生命值 = 10;
+        this.初始攻击力 = 1;
+        this.初始防御力 = 0;
+        this.生命值成长 = 1;
+        this.攻击力成长 = 1;
+        this.防御力成长 = 1;
+    }
 
 }
